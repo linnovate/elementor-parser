8482a387315fd990d801258af7d23d97fc68c526
aron <astein@linnovate.net>
2018-09-17 17:26:17 +0300

    Fix to can use only as canvas & add clean route "elementor/content/%post_id%"

--- elementor_outside/elementor_outside.php
+++ elementor_outside/elementor_outside.php
@@ -17,23 +17,39 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
-add_action( 'init', 'elementor_outside_pages' );
+add_action( 'init', 'elementor_outside' );
 
-function elementor_outside_pages() {
-	// add_rewrite_tag('%page%','([^/]+)', 'pagename=');
-	add_permastruct('elementor_content','/elementor/content/%page_id%/',false);
+function elementor_outside() {
+	// add_rewrite_tag('%elementor_content_id%','([0-9]+)', 'elementor_content_id=');
 
-// 	global $wp_rewrite;
-// $projects_structure = '/projects/%year%/%monthnum%/%day%/%projects%/';
-// $wp_rewrite->add_rewrite_tag("%projects%", '([^/]+)', "project=");
-// $wp_rewrite->add_permastruct('projects', $projects_structure, false);
+	add_permastruct('elementor_content','/elementor/content/%post_id%',false);
+	add_permastruct('elementor_edit','elementor/edit/%post_id%',false);
+	add_permastruct('elementor_preview','elementor/preview/%post_id%',false);
+	// add_permastruct('elementor_create','elementor/create',false);
+	add_permastruct('elementor_update','elementor/update/',false);
 
-	// add_permastruct('elementor/content','/elementor/content/%post_id%',false);
+	// add_rewrite_rule('^elementor/content/([0-9]+)/?', 'index.php?elementor_content=$matches[1]&page_id=$matches[1]', 'top');
+	add_rewrite_rule('^elementor/create', 'wp-admin/edit.php?action=elementor_new_post&post_type=page', 'top');
+	// add_rewrite_rule('^elementor/edit/([0-9]+)/?', 'wp-admin/post.php?post=$matches[1]&action=elementor', 'top');
+	// add_rewrite_rule('^elementor/update', 'admin-ajax.php', 'top');
+	// add_rewrite_rule('^elementor/preview/([0-9]+)/?', 'index.php?page_id=$matches[1]&preview=true', 'top');
 
-	add_rewrite_rule('^elementor/content/([0-9]+)/?', 'index.php?page_id=$matches[1]', 'top');
-	add_rewrite_rule('^elementor/create', '/wp-admin/edit.php?action=elementor_new_post&post_type=page', 'top');
-	add_rewrite_rule('^elementor/edit/([0-9]+)/?', 'wp-admin/post.php?post=$matches[1]&action=elementor', 'top');
-	add_rewrite_rule('^elementor/update', 'admin-ajax.php', 'top');
-	add_rewrite_rule('^elementor/preview/([0-9]+)/?', 'index.php?page_id=$matches[1]&preview=true', 'top');
+
 	flush_rewrite_rules();
+
+	// add_filter( 'show_admin_bar', '__return_false' );
+
+	add_action( 'save_post' , function($post_id) {
+		update_metadata( 'post', $post_id, '_wp_page_template', 'elementor_canvas' );
+	});
+
+	add_filter( 'elementor/documents/register_controls', function($document) {
+		$document->update_control('template', [
+			'default' => 'elementor_canvas',
+			'type' => 'hidden',
+			'options' => ['elementor_canvas' => __( 'Elementor canvas', 'elementor' )],
+		]);
+		$document->remove_control('template_default_description');
+		$document->remove_control('template_canvas_description');
+	});
 }
